1d88e99397092d16bf9515d88f8b3c10ac7d7f03
ZUBAIR <ZUBAIR@192.168.1.13>
2025-07-01 06:40:16 +0530

    added a inbuilt method

--- Assignments/src/assign/prime_num.java
+++ Assignments/src/assign/prime_num.java
@@ -23,7 +23,7 @@ public class prime_num {
         if (num <= 1) {
             return false;
         }
-        for (int i = 2; i*i <= num; i++) {
+        for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0) {
                 return false;
             }
