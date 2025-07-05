bdf715b0bf8fdb501ab740e5c1f37d1582a4b8ff
Zubair Pathan <pathanzubairaziz@gmail.com>
2025-07-05 14:39:06 +0530

    Changed the folder name

---
+++ Assignments_1/src/assign/even_odd.java
@@ -0,0 +1,21 @@
+package assign;
+
+import java.util.Scanner;
+
+public class even_odd {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+
+        scanner.close();
+    }
+}
---
+++ Assignments_1/src/assign/voting.java
@@ -0,0 +1,21 @@
+package assign;
+
+import java.util.Scanner;
+
+public class voting {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        if (age >= 18) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+
+        scanner.close();
+    }
+}
