e484a72205cfd0fb1da60f1fe0f23acacb99f462
Zubair Pathan <pathanzubairaziz@gmail.com>
2025-07-10 22:33:48 +0530

    heirarchical inheritance

---
+++ Inheritance_Code/src/com/HeirarchicalInheritance/Car.java
@@ -0,0 +1,15 @@
+package com.HeirarchicalInheritance;
+
+public class Car extends Vehicle{
+	
+	int noofseats;
+	int noofwheels;
+	
+	
+	public void fuelType() {
+		System.out.println("Fuel  Type = Petrol / CNG");
+	}
+	
+	
+
+}
---
+++ Inheritance_Code/src/com/HeirarchicalInheritance/Vehicle.java
@@ -0,0 +1,12 @@
+package com.HeirarchicalInheritance;
+
+public class Vehicle {
+	public void speed() {
+		System.out.println("Speed of Vehicle = 50");
+	}
+	
+	public void engine() {
+		System.out.println("Engine");
+	}
+
+}
